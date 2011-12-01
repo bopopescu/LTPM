de7eaca444712c4086a195bede5db68772627fc4
David Stolarsky <david.stolarsky@gmail.com>
2011-11-30 22:40:33 -0500

    visualizing berkeley segmentation. awesome.

--- VisualizeSegmentation/src/LTPMApp.cpp
+++ VisualizeSegmentation/src/LTPMApp.cpp
@@ -16,7 +16,13 @@ LTPMApp::LTPMApp(int argc, char** argv)
 	segFilename = argv[2];
 }
 
-vector<int> segFile2Vector(string segFilename)
+typedef struct Segmentation
+{
+  int maxLabel;
+  vector<int> labels;
+} Segmentation;
+
+Segmentation segFile2Vector(string segFilename)
 {
   vector<int> segData;
   ifstream file (segFilename.c_str(), ios::in|ios::binary|ios::ate);
@@ -45,8 +51,97 @@ vector<int> segFile2Vector(string segFilename)
   }
   else cout << "Unable to open file";
   //return 0;
-	
-	return segData;	
+
+
+  vector<int>::iterator currentInt = segData.begin();
+  
+  int dim = *currentInt;
+  currentInt++;
+  cout << "dim: " << dim << endl;
+  
+  int prod = 1;
+  vector<int> dim_vals;
+  for(int d = 0; d < dim; d++)
+  {
+    prod *= *currentInt;
+    cout << *currentInt << ", ";
+    dim_vals.push_back(*currentInt);
+    currentInt++;
+  }
+  cout << "prod: " << prod << endl;
+
+  //vector<int> realSegData;
+  Segmentation seg;
+  //int realMax = *currentInt;
+  seg.maxLabel = *currentInt;
+  for(int p = 0; p < prod; p++)
+  {
+    seg.maxLabel = MAX(*currentInt, seg.maxLabel);
+    seg.labels.push_back(*currentInt);
+    currentInt++;
+  }
+  cout << "real max: " << seg.maxLabel << endl;
+
+
+
+  return seg;
+
+
+/*
+  vector<unsigned int> d;
+
+
+  ///fid = fopen(filename,'r');
+  ifstream file (segFilename.c_str(), ios::in|ios::binary|ios::ate);
+  if (file.is_open())
+  {
+    ///dim = fread(fid,1,'uint');
+    unsigned int dim;
+    file.read((char*)&dim, sizeof(unsigned int));
+
+    ///for i = 1:dim
+    ///  d(i) = fread(fid,1,'uint');
+    ///end;
+    for(int i = 0; i < dim; i++)
+    {
+      unsigned int intI;
+      file >> intI;
+      d.push_back(intI);
+    }
+
+
+/*
+    [B,count] = fread(fid,prod(d),'int');
+    B = reshape(B,fliplr(d));
+
+dim = fread(fid,1,'uint');
+
+if (~feof(fid)) %collection of arrays.
+  A{1} = B;
+  ct = 2;
+  while (~feof(fid))
+    for i = 1:dim
+      d(i) = fread(fid,1,'uint');
+    end;
+    [B,count] = fread(fid,prod(d),'int');
+    A{ct} = reshape(B,fliplr(d));
+    ct = ct + 1;
+    dim = fread(fid,1,'uint');
+  end;
+else %else a single array 
+  A = B;
+end;
+
+*/
+
+/*
+    cout << "dim: " << dim << endl << "d: ";
+    for(int i = 0; i < d.size(); i++)
+      cout << d[i] << ", " << endl;
+*/
+
+ // }
+
 }
 
 //--------------------------------------------------------------
@@ -65,15 +160,22 @@ void LTPMApp::setup(){
 //	cam.setDistance(100);
 
 	image.loadImage(imageFilename);
-	segImage.allocate(image.width, image.height, OF_IMAGE_GRAYSCALE);
-	vector<int> segData = segFile2Vector(segFilename);
+	segImage.clear();
+	segImage.allocate(image.width, image.height, OF_IMAGE_COLOR);
+	Segmentation seg = segFile2Vector(segFilename);
+        
 	for(int x = 0; x < image.width; x++)
 	{
-		for(int y = 0; y < image.width; y++)
+		for(int y = 0; y < image.height; y++)
 		{
-			segImage.setColor(x, y, ofColor(segData[y*image.width+x]));
+			float val = float(seg.labels[x*image.height+y]) / float(seg.maxLabel);
+			ofColor color(int(val * 255));
+			//cout << segData[x*image.width+y] << ", ";
+			segImage.setColor(x, y, color);
 		}
 	}
+	segImage.update();
+	cout << endl;
 	
 }
 
