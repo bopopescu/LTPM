96b86a0e64f6dea4a87b1212a249f21f48b32117
David Stolarsky <david.stolarsky@gmail.com>
2011-12-04 18:15:53 -0500

    begone old

--- App/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <ofAppGlutWindow.h>
-#include "LTPMApp.h"
-
-int main(int argc, char** argv) {
-	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 640, 480, OF_WINDOW);
-	ofRunApp(new LTPMApp(argc, argv));
-}
