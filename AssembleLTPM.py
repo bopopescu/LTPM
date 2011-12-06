18d2d3fb9e207a3ea414dadf311968e93fa30d0f
David Stolarsky <david.stolarsky@gmail.com>
2011-12-05 20:56:26 -0500

    closer to working

--- AssembleLTPM.py
+++ AssembleLTPM.py
@@ -6,34 +6,45 @@ import MySQLdb
 
 if len(sys.argv) < 4:
 	print "Usage: "+sys.argv[0]+" <LTPM_name> <rows> <cols>"
+	exit(0)
+targetImagePath = sys.argv[1]
+rows = int(sys.argv[2])
+cols = int(sys.argv[3])
 
-db=MySQLdb.connect(username="LTPM",db="LTPM")
+basename = os.path.basename(sys.argv[1])
+LTPMName, ext = os.path.splitext(basename)
+
+db=MySQLdb.connect(user="LTPM", db="LTPM")
 c=db.cursor()
 
-originalTargetImage = Image.open(sys.argv[1])
+originalTargetImage = Image.open(targetImagePath)
 width  = originalTargetImage.size[0]
 height = originalTargetImage.size[1]
 LTPMImage = Image.new("RGB", originalTargetImage.size)
 
-rows = sys.argv[2]
-cols = sys.argv[3]
 
-destPath = sys.argv[4]
+#destPath = sys.argv[4]
 
 cellWidth = originalTargetImage.size[0] / cols
 cellHeight = originalTargetImage.size[1] / rows
 
-basename = os.path.basename(sys.argv[1])
-file, ext = os.path.splitext(basename)
 
 for row in range(0,rows):
 	for col in range(0,cols):
+		#target_tile = LTPMName+'_'+str(row)+'_'+str(col)+'.realseg'
+		target_tile = "Data/Images/realsegFiles/grid-adam_0_0.jpg.8.realseg"
 		c.execute("""SELECT candidate_image FROM scores WHERE target_tile = %s ORDER BY score DESC LIMIT 1""", (target_tile,))
+		winning_candidate_image = c.fetchone()[0]
 		x = col*cellWidth
 		y = row*cellHeight
-		candidateImage = blah
-		LTPMImage.paste(candidateImage, (x, y))
+		print "Pasting " + winning_candidate_image + " at ("+str(x)+", "+str(y)+")"
+		#candidateImage = blah
+		#LTPMImage.paste(candidateImage, (x, y))
 
-im.save("Data/LTPMs/"+LTPMName+".png")
+c.close()
+db.close()
+
+
+LTPMImage.save("Data/"+LTPMName+"/"+LTPMName+"_LTPM.png")
 
 exit(0)
